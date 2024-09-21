0f2647fc99e43adf3ed7829e8baed9712510adc5
Mvrtn-design <martinmochi56@gmail.com>
2024-09-21 12:08:28 +0200

    reestructore of D3

--- src/main/java/com/example/D3/Main.java
+++ src/main/java/com/example/D3/Main.java
@@ -2,37 +2,46 @@ package com.example.D3;
 
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n\n-----       WELCOME TO THE DAY 3 OF IRONHACK    -----\n\n");
-        System.out.print("Choose an option(1/4): ");
-        int option = scanner.nextInt();
-        switch (option) {
-            case 1:
-            // In this exercise, you will practice working with arrays and lists in Java.
-                new Arrays1();
-                break;
 
-            case 2:
-                
-                break;
+        Scanner scan = new Scanner(System.in);
+        boolean correctOption;
 
-            case 3:
-          
-                break;
+        do {
+            correctOption = true;// opcion por defecto
+            System.out.println("\n\n-----       WELCOME TO THE DAY 3 OF IRONHACK    -----\n\n");
+            System.out.print("Choose an option(1/4) or 0 for the additional one: ");
+            int exercise = scan.nextInt();
+            switch (exercise) {
+                case 1:
+                    // practice working with arrays and lists in Java.
+                    new Arrays1();
+                    break;
 
-            case 4:
-                
-                break;
+                case 2:
+                    // Apply the operations learned to manipulate arrays and lists in Java.
+                    new Operations_and_DataStructures(23, "Hello");
+                    break;
 
-            default:
-                System.out.println("Invalid option");
-                break;
-        }
+                case 3:
+                    new ThirdExercise();
+                    break;
 
+                case 4:
+                    new exercise4();
+                    break;
+                case 0:
+                    new Aditional();
+                    break;
+
+                default:
+                    System.out.println("Wrong option");
+                    correctOption = false;
+                    break;
+            }
+        } while (!correctOption);
         System.out.println("\n\n-----      END OF THE PROGRAM    -----\n\n");
     }
-    
+
 }
--- src/main/java/com/example/D3/exercise4.java
+++ src/main/java/com/example/D3/exercise4.java
@@ -1,9 +1,17 @@
 package com.example.D3;
 
 public class exercise4 {
+    exercise4() {
+        System.out.println("----    Exercise 4    -----\n\n");
+        System.out.println("Area of rectangle: " + calculateArea(123, 456));
+        System.out.println("Full name: " + getFullName("Sanobar", "Fatema"));
+        int[] littleArray = { 123, 456 };
+        System.out.println("Pre swapping values: " + littleArray[0] + " " + littleArray[1]);
+        swap(littleArray);
+        System.out.println("Post swapping values: " + littleArray[0] + " " + littleArray[1]);
 
-    exercise4(){
     }
+
     private static double calculateArea(double lenghtParametre, double widthParametre) {
         return lenghtParametre * widthParametre;
     }
@@ -17,15 +25,4 @@ public class exercise4 {
         twoIntegersArray[0] = twoIntegersArray[1];
         twoIntegersArray[1] = aux;
     }
-
-    public void runFourthExercise() {
-        System.out.println("----    Exercise 4    -----\n\n");
-        System.out.println("Area of rectangle: " + calculateArea(123, 456));
-        System.out.println("Full name: " + getFullName("Sanobar", "Fatema"));
-        int[] littleArray = { 123, 456 };
-        System.out.println("Pre swapping values: " + littleArray[0] + " " + littleArray[1]);
-        swap(littleArray);
-        System.out.println("Post swapping values: " + littleArray[0] + " " + littleArray[1]);
-    }
-
 }
